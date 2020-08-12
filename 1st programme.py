a4d8a15e8b71d672b36293e66adadce39ea56486
Walid3543 <68558275+Walid3543@users.noreply.github.com>
2020-08-12 21:24:23 +0600

    Add files via upload

--- 1st programme.py
+++ 1st programme.py
@@ -1,9 +1,14 @@
-print( "Enter two variables: ")
-a = input()
-b = input()
-temp = 0
-temp = a
-a = b
-b = temp
-print("a= ", a)
-print("b= ", b)
+import math
+print("Enter the coefficients: ")
+astr = input()
+bstr = input()
+cstr = input()
+a = int(astr)
+b = int(bstr)
+c = int(cstr)
+D = math.sqrt(b*b-4*c)
+x1 = (-b+D)/(2*a)
+x2 = (-b-D)/(2*a)
+print(x1)
+print(x2)
+
