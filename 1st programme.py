33ea6dc6b390c098f546b50ea074a0dd48cf12cd
Walid3543 <68558275+Walid3543@users.noreply.github.com>
2020-08-12 21:55:02 +0600

    Add files via upload

--- 1st programme.py
+++ 1st programme.py
@@ -1,14 +1,4 @@
-import math
-print("Enter the coefficients: ")
-astr = input()
-bstr = input()
-cstr = input()
-a = int(astr)
-b = int(bstr)
-c = int(cstr)
-D = math.sqrt(b*b-4*c)
-x1 = (-b+D)/(2*a)
-x2 = (-b-D)/(2*a)
-print(x1)
-print(x2)
-
+x = input("Enter the Temperature in celsius: ")
+c = float(x)
+f = (9/5)*c+32
+print(f)
