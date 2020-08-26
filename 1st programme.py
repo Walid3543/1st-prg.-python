a66e66206b48e2952f75ccb3b1222da3037d3b8e
Walid3543 <68558275+Walid3543@users.noreply.github.com>
2020-08-26 22:12:46 +0600

    Add files via upload

--- 1st programme.py
+++ 1st programme.py
@@ -1,8 +1,7 @@
-print('enter your command')
-robot_move = input()
-if robot_move == 'front':
-   print('Moving Front')
-elif robot_move == 'back':
-    print('Moving back')
-else:
-    print('Stand Still')
+while True:
+    print('Please Enter your Name!')
+    name = input()
+    if name == 'your name':
+       break
+
+print('Thank you')
